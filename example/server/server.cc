d429a470a7eea2e9a5a54d5f4ed30e455f60dd0a
sails <sailsxu@gmail.com>
2015-04-25 19:13:47 +0800

    cpplint

--- example/server/server.cc
+++ example/server/server.cc
@@ -15,86 +15,85 @@
 
 
 typedef struct {
-    char msg[100];
+  char msg[100];
 } __attribute__((packed)) EchoStruct;
 
 namespace sails {
 sails::log::Logger serverlog(sails::log::Logger::LOG_LEVEL_DEBUG,
-				  "./log/server.log", sails::log::Logger::SPLIT_DAY);
+                             "./log/server.log", sails::log::Logger::SPLIT_DAY);
 }
 
 
 class TestServer : public sails::net::EpollServer<EchoStruct> {
-public:
-    TestServer() {
-	
-    }
-    ~TestServer() {
-	
+ public:
+  TestServer() {
+  }
+  ~TestServer() {
+  }
+
+  EchoStruct* Parse(std::shared_ptr<sails::net::Connector> connector) {
+    int read_able = connector->readable();
+    if (read_able == 0) {
+      return NULL;
     }
 
-    EchoStruct* Parse(std::shared_ptr<sails::net::Connector> connector){
-	int read_able = connector->readable();
-	if(read_able == 0) {
-	    return NULL;
-	}
+    EchoStruct *data =
+        reinterpret_cast<EchoStruct*>(malloc(sizeof(EchoStruct)));
+    memset(data, '\0', sizeof(EchoStruct));
+    strncpy(data->msg, connector->peek(), read_able);
+    connector->retrieve(read_able);
+    return data;
+  }
 
-	EchoStruct *data = (EchoStruct*)malloc(sizeof(EchoStruct));
-	memset(data, '\0', sizeof(EchoStruct));
-	strncpy(data->msg, connector->peek(), read_able);
-	connector->retrieve(read_able);        
-	return data;
-    }
-    
-    void handle(const sails::net::TagRecvData<EchoStruct> &recvData) {
-//     CloseConnector(recvData.ip, recvData.port, recvData.uid, recvData.fd);
+  void handle(const sails::net::TagRecvData<EchoStruct> &recvData) {
+    // CloseConnector(recvData.ip, recvData.port, recvData.uid, recvData.fd);
 
-	printf("uid:%u, ip:%s, port:%d, msg:%s\n", recvData.uid, recvData.ip.c_str(), recvData.port, recvData.data->msg);
-	std::string buffer = std::string(recvData.data->msg);
-        send(buffer, recvData.ip, recvData.port, recvData.uid, recvData.fd);
-        static uint32_t testnum = 0;
-        testnum++;
-        sails::net::ExtData data;
-        data.u32 = testnum;
-    }
+    printf("uid:%u, ip:%s, port:%d, msg:%s\n",
+           recvData.uid, recvData.ip.c_str(),
+           recvData.port, recvData.data->msg);
+    std::string buffer = std::string(recvData.data->msg);
+    send(buffer, recvData.ip, recvData.port, recvData.uid, recvData.fd);
+    static uint32_t testnum = 0;
+    testnum++;
+    sails::net::ExtData data;
+    data.u32 = testnum;
+  }
 };
 
 
 bool isRun = true;
 
 void sails_signal_handle(int signo, siginfo_t *, void *) {
-    switch(signo) {
-	case SIGINT:
-	{
-	    isRun = false;
-	}
-    }
+  switch (signo) {
+    case SIGINT:
+      {
+        isRun = false;
+      }
+  }
 }
 
 
 
 
-int main(int argc, char *argv[])
-{
+int main(int, char *[]) {
+  // signal kill
+  struct sigaction act;
+  act.sa_sigaction = sails_signal_handle;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
+  if (sigaction(SIGINT, &act, NULL) == -1) {
+    perror("sigaction error");
+    exit(EXIT_FAILURE);
+  }
 
-    // signal kill
-    struct sigaction act;
-    act.sa_sigaction = sails_signal_handle;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
-    if(sigaction(SIGINT, &act, NULL) == -1) {
-	perror("sigaction error");
-	exit(EXIT_FAILURE);
-    }
+  TestServer server;
+  server.Init(8000, 2, 10, 1, true);
 
-    TestServer server;
-    server.Init(8000, 2, 10, 1,true);
+  while (isRun) {
+    sleep(2);
+  }
 
-    while(isRun) {
-	sleep(2);
-    }
-    
-    server.Stop();
+  server.Stop();
 
-    return 0;
+  return 0;
 }
